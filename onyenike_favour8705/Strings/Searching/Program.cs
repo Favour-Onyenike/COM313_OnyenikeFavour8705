5bb69de55a3106c893cdd54bf07c01fbf2e6a06e
Favour Onyenike <92091388+onyenikefavour8@users.noreply.github.com>
2024-10-07 09:45:50 +0100

    serching and number formating

--- onyenike_favour8705/Strings/Searching/Program.cs
+++ onyenike_favour8705/Strings/Searching/Program.cs
@@ -9,12 +9,25 @@ Console.WriteLine($"{teststr.Contains("fox", StringComparison.CurrentCultureIgno
 
 // TODO: StartsWith and EndsWith determine if a string starts 
 // or ends with a given test string
+Console.WriteLine($"{teststr.StartsWith("the")}");
+Console.WriteLine($"{teststr.StartsWith("the", StringComparison.CurrentCultureIgnoreCase)}");
+Console.WriteLine($"{teststr.StartsWith("dog")}");
+Console.WriteLine($"{teststr.StartsWith("dog", StringComparison.CurrentCultureIgnoreCase)}");
+
+
 
 
 // TODO: IndexOf, LastIndexOf finds the index of a substring
+Console.WriteLine($"{teststr.IndexOf("the")}");
+Console.WriteLine($"{teststr.IndexOf("the", StringComparison.CurrentCultureIgnoreCase)}");
+Console.WriteLine($"{teststr.LastIndexOf("the")}");
 
 
 // TODO: Determining empty, null, or whitespace
 string str1 = null;
 string str2 = "   ";
 string str3 = String.Empty;
+
+Console.WriteLine($"{string.IsNullOrEmpty(str1)}");
+Console.WriteLine($"{string.IsNullOrWhiteSpace(str2)}");
+Console.WriteLine($"{string.IsNullOrEmpty(str3)}");
