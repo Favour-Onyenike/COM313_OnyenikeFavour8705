09e66e9e57bdc7f8a42d41e11fe50c4fb1d0bfe9
Favour Onyenike <92091388+onyenikefavour8@users.noreply.github.com>
2024-10-14 09:56:35 +0100

    file manipulation 14/12/2024

--- onyenike_favour8705/Files/ReadWriteFiles/Program.cs
+++ onyenike_favour8705/Files/ReadWriteFiles/Program.cs
@@ -6,14 +6,21 @@ const string filename = "TestFile.txt";
 
 // TODO 1: WriteAllText overwrites a file with the given content
 if (!File.Exists(filename)) {
-
+   File.WriteAllText(filename, "this is a text file");
 }
 
 // TODO 3: AppendAllText adds text to an existing file
-
+File.AppendAllText(filename, "this text gets appended to the file");
 
 // TODO 4: A FileStream can be opened and written to until closed
-
+using(StreamWriter sw= File.AppendText(filename)){
+    sw.WriteLine("line 1");
+    sw.WriteLine("line 2");
+    sw.WriteLine("line 3");
+}
 
 // TODO 2: ReadAllText reads all the content from a file
+string content;
+content = File.ReadAllText(filename);
+Console.WriteLine(content);
 
